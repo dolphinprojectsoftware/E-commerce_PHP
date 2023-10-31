6ea730ea5f9c1d382073c6fef01e0b956bfbda4b
Francisco Illescas <mindencoded@users.noreply.github.com>
2023-10-31 17:27:02 -0500

    Add Modules Package

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $path = module_path('Core');
     return view('welcome');
 });
+
+
+Route::get('/home', [HomeController::class, 'index']);
